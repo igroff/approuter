9d165123475bd7c70a3080163d472abcb56a828a
Ian Groff <ian.groff@you_should_know.com>
2012-12-14 08:57:02 -0600

    changez

--- bin/start.sh
+++ bin/start.sh
@@ -1,8 +1,16 @@
 #! /usr/bin/env bash
 
+MANAGED_ROOT=`pwd`/managed
+ALTERNATE_ROOT=${MANAGED_ROOT}/alternates
+LOG_DIR=${MANAGED_ROOT}/var/log
+RUN_DIR=${MANAGED_ROOT}/var/run
+START_CMD_TEMPLATE=`cat ${MANAGED_ROOT}/starter.conf`
+
+mkdir -p ${LOG_DIR}
+mkdir -p ${RUN_DIR}
+
 spushd(){ pushd $1 > /dev/null 2>&1; }
 spopd(){ popd $1 > /dev/null 2>&1 ; }
-
 
 clone_repo_as_needed() {
     # $1 - the directory to check
@@ -23,14 +31,14 @@ clone_repo_as_needed() {
 }
 
 setup_managed_repositories(){
-    clone_repo_as_needed ./managed/prod/instance1 `cat ./managed/repo.conf`
-    clone_repo_as_needed ./managed/prod/instance2 `cat ./managed/repo.conf`
+    clone_repo_as_needed ${MANAGED_ROOT}/prod/instance1 `cat ./managed/repo.conf`
+    clone_repo_as_needed ${MANAGED_ROOT}/prod/instance2 `cat ./managed/repo.conf`
 
     # alternates
     while read alternate 
     do
         echo "initializing alternate ${alternate}"
-        ALTERNATE_DIR=./managed/alternates/${alternate}
+        ALTERNATE_DIR=${ALTERNATE_ROOT}/${alternate}
         clone_repo_as_needed ${ALTERNATE_DIR} `cat ./managed/repo.conf`
         spushd ${ALTERNATE_DIR}
         git checkout ${alternate}
@@ -38,4 +46,20 @@ setup_managed_repositories(){
     done < ./managed/alternates.conf
 }
 
+start_instance(){
+    # $1 path to instance
+    # $2 port
+    spushd $1
+    DIR_NAME=`basename $1`
+    START_CMD=`echo "${START_CMD_TEMPLATE}" | sed -e s/{{port}}/$2/g`
+    START_CMD="${START_CMD}"
+    echo "starting: ${START_CMD}"
+    ${START_CMD} > ${LOG_DIR}/${DIR_NAME}_$2.log 2>&1 & 
+    echo $! > "${RUN_DIR}/$2.pid"
+}
 setup_managed_repositories;
+# start each of our application instances
+# prod ports will be 9001, 9002
+# alternates we'll start at 9050
+start_instance ${MANAGED_ROOT}/prod/instance1 9001
+start_instance ${MANAGED_ROOT}/prod/instance2 9002
