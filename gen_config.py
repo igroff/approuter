d6b6963652fc898e015585b6b1820b7ecdf28ae6
Ian Groff <ian.groff@you_should_know.com>
2012-12-20 22:24:38 -0600

    removing un needed files

--- gen_config.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env python
-import yaml
-import pystache
-
-approuter_config = yaml.load(open('./etc/approuter.yaml'))
-print(approuter_config)
-print(pystache.render(open('./templates/nginx.conf').read(), approuter_config))
