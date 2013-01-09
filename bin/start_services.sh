b0a79205acaab650a76a5378742a9d451274dadd
Ian Groff <ian.groff@you_should_know.com>
2013-01-08 19:43:53 -0600

    removing some files that are no longer needed

--- bin/start_services.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-while read line
-do
-    START_CMD=`echo $line | sed -e s/{{port}}/9000/g`
-    ./bin/restart_on_exit.sh "${START_CMD}" &
-done < "./etc/services"
--- bin/start_smtp.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env bash
-./build_output/sbin/nginx -c `pwd`/templates/nginx.smtp.conf
-./bin/auth_server.py start
